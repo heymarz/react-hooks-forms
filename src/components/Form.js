41488ee9b33ea376796724de5d30e95977d186c8
Marlena Tsang <Marlena345@gmail.com>
2021-12-01 22:09:48 -0500

    commit tree f16bf832896d8bbf85dca000e4cc40317c729b4c

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form(props) {
+  
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={props.firstName} />
+      <input type="text" onChange={handleLastNameChange} value={props.lastName} />
       <button type="submit">Submit</button>
     </form>
   );
